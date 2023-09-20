780e23ee453425e5e4dfca044fc6e92c2d272bd3
abektimi <142258964+abektimi@users.noreply.github.com>
2023-09-20 16:16:41 +0200

    loop 2

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mburgler <mburgler@student.42.fr>          +#+  +:+       +#+        */
+/*   By: abektimi <abektimi@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 22:38:40 by mburgler          #+#    #+#             */
-/*   Updated: 2023/09/20 16:03:09 by mburgler         ###   ########.fr       */
+/*   Updated: 2023/09/20 16:16:33 by abektimi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,34 @@
 int	main(int argc, char **argv, char **env)
 {
 	t_msc *msc;
+	char	*input;
 	if (argc != 1 || !argv[0] || !env)
 		return (-1);
 	msc = ft_calloc(1, sizeof(t_msc));
 	if (!msc)
 		return (-1);
 	while(loop == true)
+	{
+		input = readline("MyShell> "); // Display a prompt and read user input
+
+        if (!input) {
+            // If input is NULL (e.g., user pressed Ctrl+D), exit the loop
+            printf("\n");
+            break;
+        }
+
+        if (strcmp(input, "exit") == 0) {
+            // If user enters "exit", exit the shell
+            free(input);
+            break;
+        }
+
+        // Process the user command here
+        printf("You entered: %s\n", input);
+
+		add_history(input);
+		free(input);
+	}
 	
 
 	
