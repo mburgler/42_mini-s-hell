dea7bc1976893ea3c3074c7b05ca4456e0a727bb
Matteo Bürgler <107435512+mburgler@users.noreply.github.com>
2023-10-18 16:12:17 +0200

    commit 2023-10-18 16:12:17 by mburgler

--- src/cmd_utils2.c
+++ src/cmd_utils2.c
@@ -1,70 +1,70 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   cmd_utils2.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: abektimi <abektimi@student.42vienna.com    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/08 19:47:20 by abektimi          #+#    #+#             */
-/*   Updated: 2023/10/13 00:37:37 by abektimi         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   cmd_utils2.c                                       :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: abektimi <abektimi@student.42vienna.com    +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2023/10/08 19:47:20 by abektimi          #+#    #+#             */
+// /*   Updated: 2023/10/13 00:37:37 by abektimi         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-#include "../inc/minishell.h"
+// #include "../inc/minishell.h"
 
-//counts and returns the number of sections
-//in t_list *lex which are separated by pipes
-int nb_of_cmds(t_list *lst)
-{
-	int	ret;
-
-	ret = 1;
-	if (!lst)
-		return (0);
-	while (lst)
-	{
-		if (lst->token_status == IS_PIPE)
-			ret++;
-		lst = lst->next;
-	}
-	return (ret);
-}
-
-//separates the t_list *lex into parts which
-//set_full_cmd() can use
-// char	**get_full_cmd(t_list *lst)
+// //counts and returns the number of sections
+// //in t_list *lex which are separated by pipes
+// int nb_of_cmds(t_list *lst)
 // {
-// 	int				i;
-// 	static int		p_count;
-// 	static t_list	*tmp;
+// 	int	ret;
 
-	if (!lst)
-		return (NULL);
-	i = 0;
-	if (cmds_done == 0)
-	{
-		tmp = lst;
-		cmds_done == 1;
-	}
-	while (tmp)
-	{
-		if (tmp->token_status != IS_PIPE)
-			i++;
-		else
-		{
-			tmp = tmp->next;
-			break;
-		}
-		tmp = tmp->next;
-	}
-	if (tmp == NULL)
-		cmds_done = 0;	
-	return (set_full_cmd(lst, i));
-}
+// 	ret = 1;
+// 	if (!lst)
+// 		return (0);
+// 	while (lst)
+// 	{
+// 		if (lst->token_status == IS_PIPE)
+// 			ret++;
+// 		lst = lst->next;
+// 	}
+// 	return (ret);
+// }
 
-//assembles the full_cmd variable
-//of each t_cmd node from t_list *lex
-char	**set_full_cmd(t_list *lst, int words)
-{
+// //separates the t_list *lex into parts which
+// //set_full_cmd() can use
+// // char	**get_full_cmd(t_list *lst)
+// // {
+// // 	int				i;
+// // 	static int		p_count;
+// // 	static t_list	*tmp;
+
+// 	if (!lst)
+// 		return (NULL);
+// 	i = 0;
+// 	if (cmds_done == 0)
+// 	{
+// 		tmp = lst;
+// 		cmds_done == 1;
+// 	}
+// 	while (tmp)
+// 	{
+// 		if (tmp->token_status != IS_PIPE)
+// 			i++;
+// 		else
+// 		{
+// 			tmp = tmp->next;
+// 			break;
+// 		}
+// 		tmp = tmp->next;
+// 	}
+// 	if (tmp == NULL)
+// 		cmds_done = 0;	
+// 	return (set_full_cmd(lst, i));
+// }
+
+// //assembles the full_cmd variable
+// //of each t_cmd node from t_list *lex
+// char	**set_full_cmd(t_list *lst, int words)
+// {
 	
-}
+// }
